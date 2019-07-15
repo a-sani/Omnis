7ed387f653323ba38a191d9321a5b4b6a6c5b422
Piercson Thomas <piie1996@gmail.com>
2019-07-15 10:25:46 -0700

    Revert "login created"
    This reverts commit 867647d3f7f440fc8d76546dca11a947a7e1b6ae.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,9 +10,8 @@ class UsersController < ApplicationController
   	def create
     	@user = User.new(user_params)
     	if @user.save
-        log_in @user
     		flash[:success] = "Welcome to the Homepage!"
-      	redirect_to @user #user url
+      		redirect_to @user #user url
     	else
       		render 'new'
     	end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "index#startup"
 
@@ -9,8 +7,5 @@ Rails.application.routes.draw do
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
   resources :users
 end
