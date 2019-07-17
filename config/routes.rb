f5de93d0981caac1346e3a1c3052b224a7ec64cd
sainii <ahsaini@sfu.ca>
2019-07-16 21:22:24 -0700

    static page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-	root 'users#new'
-	
   get 'static_pages/home'
 
   get 'static_pages/help'
 
+	root 'users#new'
+	
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
