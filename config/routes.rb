9427b10e4ef23f169cd78071b3b4df72e0dcb5cf
sainii <ahsaini@sfu.ca>
2019-07-20 15:56:38 -0700

    signup form create

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   	get  '/help',    to: 'static_pages#help'
 	get  '/signup',  to: 'users#new'
 	
+	resources: users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
