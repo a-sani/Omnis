2607d8e30648679a1ffd0576aa71e83428296fca
sainii <ahsaini@sfu.ca>
2019-07-21 18:29:22 -0700

    reset pw

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,18 @@ class User < ApplicationRecord
     UserMailer.password_reset(self).deliver_now
   end
 
+  private
+
+    # Converts email to all lower-case.
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    # Creates and assigns the activation token and digest.
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
   # # Returns a random token.
   # def User.new_token
   #   SecureRandom.urlsafe_base64
