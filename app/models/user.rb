87d281409810bf47bf10d20636358ce3c0f71f1d
sainii <ahsaini@sfu.ca>
2019-07-21 19:21:01 -0700

    email working?

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,10 +42,10 @@ class User < ApplicationRecord
     end
 
     # Creates and assigns the activation token and digest.
-    def create_activation_digest
-      self.activation_token  = User.new_token
-      self.activation_digest = User.digest(activation_token)
-    end
+    # def create_activation_digest
+    #   self.activation_token  = User.new_token
+    #   self.activation_digest = User.digest(activation_token)
+    # end
   # # Returns a random token.
   # def User.new_token
   #   SecureRandom.urlsafe_base64
